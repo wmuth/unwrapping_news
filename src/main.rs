acbc30463a6853a93d0777ebd1ae7c4722a55ab0
wmuth <98701865+wmuth@users.noreply.github.com>
2024-04-10 11:19:13 +0200

    Basic setup

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
-fn main() {
-    println!("Hello, world!");
+use std::sync::atomic::{AtomicU32, Ordering};
+
+use actix_web::{get, web, App, HttpResponse, HttpServer, Responder};
+
+struct State {
+    count: AtomicU32,
+}
+
+#[get("/")]
+async fn hello() -> impl Responder {
+    HttpResponse::Ok().body("Hello World!")
+}
+
+#[get("/count")]
+async fn count(data: web::Data<State>) -> impl Responder {
+    data.count.fetch_add(1, Ordering::Relaxed);
+    HttpResponse::Ok().body(format!("Count is {}", data.count.load(Ordering::Relaxed)))
+}
+
+#[actix_web::main]
+async fn main() -> Result<(), std::io::Error> {
+    let data = web::Data::new(State {
+        count: AtomicU32::new(0),
+    });
+
+    HttpServer::new(move || {
+        App::new()
+            .app_data(data.clone())
+            .service(hello)
+            .service(count)
+    })
+    .bind(("localhost", 8080))?
+    .run()
+    .await
 }
